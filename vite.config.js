b9233da1fd433d6403e7ff251bc733c5cc57c83d
Giovani G <giovanigsilva@gmail.com>
2025-09-21 23:21:47 -0300

    Configure Vite server settings for external access

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'testedeploy-kvcl6zoo.b4a.run', // 🔹 adicione seu host aqui
+    ],
+    host: true, // permite conexões externas
+    port: 5173, // opcional, default do vite
+  },
 })
