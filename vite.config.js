d0bf13e86f582d548e052f00ba9bf12834957047
Giovani G <giovanigsilva@gmail.com>
2025-10-07 17:32:59 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      'testedeploy-5nh7izs9.b4a.run', // 🔹 adicione seu host aqui
+      'testedeploy-tasuihck.b4a.run', // 🔹 adicione seu host aqui
     ],
     host: true, // permite conexões externas
     port: 5173, // opcional, default do vite
